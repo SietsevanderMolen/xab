2e72569ef1266ffc3a6ede29db481f9e4d835fc5
Sietse van der Molen <sietse@vdmolen.eu>
2013-06-05 13:44:30 +0200

    add spaces to satisfy compiler
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- src/xab_events.ads
+++ src/xab_events.ads
@@ -4,9 +4,9 @@ package Xab_Events is
 
    type Listener is Interface;
    --  Registers a new listener to be notified
-   procedure Register_Listener(Handler : in Listener) is abstract;
+   procedure Register_Listener (Handler : in Listener) is abstract;
    --  Removes a listener from the notification list
-   procedure Unregister_Listener(Handler : in Listener) is abstract;
+   procedure Unregister_Listener (Handler : in Listener) is abstract;
 
    --  Notifies listeners of a new event
    --  procedure Notify_Listeners (E : in Event) is abstract;
