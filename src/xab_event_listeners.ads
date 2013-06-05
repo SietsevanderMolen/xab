65d01c5c50078f0078601a19881e60465002425e
Sietse van der Molen <sietse@vdmolen.eu>
2013-06-05 13:40:44 +0200

    rename things, add comments
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- src/xab_event_listeners.ads
+++
@@ -1,6 +0,0 @@
-package Xab_Event_Listeners is
-   
-   type Event_Listener is Interface;
-   procedure Notify_Handler is abstract;
-   
-end Xab_Event_Listeners;
---
+++ src/xab_events.ads
@@ -0,0 +1,13 @@
+package Xab_Events is
+   --  Implement event types here
+   --  ...
+
+   type Listener is Interface;
+   --  Registers a new listener to be notified
+   procedure Register_Listener(Handler : in Listener) is abstract;
+   --  Removes a listener from the notification list
+   procedure Unregister_Listener(Handler : in Listener) is abstract;
+
+   --  Notifies listeners of a new event
+   --  procedure Notify_Listeners (E : in Event) is abstract;
+end Xab_Events;
