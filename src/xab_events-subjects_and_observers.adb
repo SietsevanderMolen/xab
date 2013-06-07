3b22f25cbefe5161841c8f583c828c03597ea06c
Sietse van der Molen <sietse@vdmolen.eu>
2013-06-07 15:35:04 +0200

    remove Root_ prefix from subjects_and_observers
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- src/xab_events-subjects_and_observers.adb
+++ src/xab_events-subjects_and_observers.adb
@@ -1,5 +1,5 @@
 package body Xab_Events.Subjects_And_Observers is
-   procedure Notify (Subject : in out Root_Subject_Type'Class) is
+   procedure Notify (Subject : in out Subject_Type'Class) is
       Observer : Observer_Access := Subject.Head;
    begin
       while Observer /= null loop
@@ -11,17 +11,21 @@ package body Xab_Events.Subjects_And_Observers is
    procedure Initialize (Control : in out Observer_Control) is Observer :
       constant Observer_Access := Control.Observer.all'Unchecked_Access;
 
-      Subject : Root_Subject_Type'Class renames Observer.Subject.all;
+      Subject : Subject_Type'Class renames Observer.Subject.all;
    begin
       Observer.Next := Subject.Head;
       Subject.Head := Observer;
    end;
 
+   --  During finalization tho observer unregisters and gets removed from the
+   --  list. This guarantees there won't be any dangling reference problems.
+   --  This means we can turn off access checks with 'Unchecked_Access' (and we
+   --  have to)
    procedure Finalize (Control : in out Observer_Control) is
       Observer : constant Observer_Access :=
          Control.Observer.all'Unchecked_Access;
 
-         Subject : Root_Subject_Type'Class renames Observer.Subject.all;
+         Subject : Subject_Type'Class renames Observer.Subject.all;
 
          Prev  : Observer_Access;
          Index : Observer_Access;
