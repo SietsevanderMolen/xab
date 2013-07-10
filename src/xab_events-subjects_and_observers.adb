d9fa6ee784dc6d38b9a44bca70df23548df79a6e
Sietse van der Molen <sietse@vdmolen.eu>
2013-07-10 13:38:08 +0200

    apply gpl3 license
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- src/xab_events-subjects_and_observers.adb
+++ src/xab_events-subjects_and_observers.adb
@@ -1,3 +1,24 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                   Copyright (C) 2012-, Sietse van der Molen		     --
+--                                                                           --
+--    This file is part of XAB.			 	   	             --
+--									     --
+--    XAB is free software: you can redistribute it and/or modify            --
+--    it under the terms of the GNU General Public License as published by   --
+--    the Free Software Foundation, either version 3 of the License, or      --
+--    (at your option) any later version.                                    --
+--   		 		 	 	 	 	 	     --
+--    XAB is distributed in the hope that it will be useful,                 --
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of         --
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          --
+--    GNU General Public License for more details.                           --
+--                                                                           --
+--    You should have received a copy of the GNU General Public License      --
+--    along with XAB.  If not, see <http://www.gnu.org/licenses/>.           --
+--                                                                           --
+-------------------------------------------------------------------------------
+
 package body Xab_Events.Subjects_And_Observers is
    procedure Notify (Subject : in out Subject_T'Class) is
       Observer : Observer_Access := Subject.Head;
